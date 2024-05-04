bf9d5e07d6eb9f835647b3c2d84295b662cc24c4
ngvtho26 <nguyenvantho260204@gmail.com>
2024-05-04 14:13:59 +0700

    update giao dien recover password

--- src/components/RecoverPassword.tsx
+++ src/components/RecoverPassword.tsx
@@ -1,19 +1,52 @@
+import { FieldValues, useForm } from 'react-hook-form'
+import { CiMail } from 'react-icons/ci'
+
+import { supabase } from '~/config/supabase'
+
 function RecoverPassword() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ defaultValues: { email: '' } })
+
+  const onSubmit = async (values: FieldValues) => {
+    try {
+      const { error } = await supabase.auth.resetPasswordForEmail(values.email, {
+        redirectTo: '/update-password'
+      })
+
+      if (error) throw error
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="w-[100%] h-screen flex justify-center items-center bg-white">
       <div className="w-[400px] bg-[#eff1f2] p-5 rounded-xl">
         <h2 className="mb-5 text-center text-[#0e64f1] text-3xl font-medium">Recover password</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
-            <p className="mb-4 font-normal">
+            <p className="mb-4 font-normal italic">
               Enter your email address and we will send you a link to reset your password
             </p>
-            <input
-              id="email"
-              type="email"
-              placeholder="Enter your email address"
-              className="w-[100%] p-2 rounded-lg outline-none"
-            />
+            <div className="flex items-center gap-3 bg-white rounded-lg p-1">
+              <div className="pl-2">
+                <CiMail color="gray" size={20} />
+              </div>
+              <input
+                id="email"
+                type="email"
+                autoComplete="off"
+                placeholder="Enter your email address"
+                className="w-[100%] outline-none pt-2 pb-2 mr-2"
+                {...register('email', { required: true })}
+              />
+            </div>
+            {errors.email && (
+              <p className="text-sm text-red-500 italic mt-1 ml-1">Email is required!</p>
+            )}
           </div>
           <button
             type="submit"
--- src/components/UpdatePassword.tsx
+++ src/components/UpdatePassword.tsx
@@ -1,34 +1,107 @@
+import { FieldValues, useForm } from 'react-hook-form'
+import { supabase } from '~/config/supabase'
+
+import { IoEyeOutline, IoKeyOutline } from 'react-icons/io5'
+import { IoEyeOffOutline } from 'react-icons/io5'
+import { useState } from 'react'
+
 function UpdatePassword() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      password: '',
+      confirmPassword: ''
+    }
+  })
+
+  const onSubmit = async (values: FieldValues) => {
+    if (values.password && values.confirmPassword) {
+      if (values.password === values.confirmPassword) {
+        try {
+          const { error } = await supabase.auth.updateUser({ password: values.password })
+
+          if (error) throw error
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+  }
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword)
+  }
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword)
+  }
+
   return (
     <div className="w-[100%] h-screen flex justify-center items-center bg-white">
       <div className="w-[400px] bg-[#eff1f2] p-5 rounded-xl">
         <h2 className="mb-5 text-center text-[#0e64f1] text-3xl font-medium">Update password</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label htmlFor="password" className="block mb-1 font-medium">
               Password
+              <span className="text-red-500 ml-0.5">*</span>
             </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="**********"
-              className="w-[100%] p-2 rounded-lg outline-none"
-            />
+            <div className="flex items-center bg-white rounded-lg p-1">
+              <div className="ml-2">
+                <IoKeyOutline color="gray" size={20} />
+              </div>
+              <input
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="**********"
+                className="w-[100%] p-2 rounded-lg outline-none"
+                {...register('password', { required: true })}
+              />
+              <div onClick={togglePasswordVisibility} className="mr-2">
+                {showPassword ? (
+                  <IoEyeOutline color="gray" size={20} />
+                ) : (
+                  <IoEyeOffOutline color="gray" size={20} />
+                )}
+              </div>
+            </div>
+            {errors.password && (
+              <p className="text-red-500 italic text-sm w-[100%]">Password is required!</p>
+            )}
           </div>
           <div className="mb-4">
             <label htmlFor="confirm-password" className="block mb-1 font-medium">
               Confirm password
+              <span className="text-red-500 ml-0.5">*</span>
             </label>
-            <input
-              id="confirm-password"
-              type="password"
-              placeholder="**********"
-              className="w-[100%] p-2 rounded-lg outline-none"
-            />
+            <div className="flex items-center bg-white rounded-lg p-1">
+              <div className="ml-2">
+                <IoKeyOutline color="gray" size={20} />
+              </div>
+              <input
+                id="confirm-password"
+                type={showConfirmPassword ? 'text' : 'password'}
+                placeholder="**********"
+                className="w-[100%] p-2 rounded-lg outline-none"
+                {...register('confirmPassword')}
+              />
+              <div onClick={toggleConfirmPasswordVisibility} className="mr-2">
+                {showConfirmPassword ? (
+                  <IoEyeOutline color="gray" size={20} />
+                ) : (
+                  <IoEyeOffOutline color="gray" size={20} />
+                )}
+              </div>
+            </div>
           </div>
           <button
             type="submit"
-            className="bg-[#007bff] text-white w-[100%] p-2 rounded-lg font-semibold hover:opacity-85"
+            className="bg-[#007bff] text-white w-[100%] p-2 rounded-lg font-semibold hover:opacity-85 outline-none"
           >
             Update
           </button>
