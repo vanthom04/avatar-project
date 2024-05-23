bb17a602ae322a25b1730a5f308487bba0a63c84
NhatQuang <qag01092004@gmail.com>
2024-05-23 13:25:39 +0700

    pagination

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,9 +5,9 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-screen font-Roboto overflow-hidden">
       <Header />
-      <div className="h-[calc(100vh-80px)] flex">
+      <div className="h-[calc(100vh-80px)] flex ">
         <Navbar />
-        <main className="basis-4/5 p-4 overflow-y-auto">{children}</main>
+        <main className=" basis-full p-4 overflow-y-auto">{children}</main>
       </div>
     </div>
   )
--- src/pages/ManagerTemplates/index.tsx
+++ src/pages/ManagerTemplates/index.tsx
@@ -4,12 +4,18 @@ import { FaPlus } from 'react-icons/fa6'
 import { useTemplateModal, useUser } from '~/hooks'
 import { useQueryTemplates } from '~/queries'
 import Spinner from '~/components/Spinner'
+import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowForward } from 'react-icons/io'
 import TemplateTableRow from './TemplateTableRow'
 import TemplateTableEmptyRow from './TemplateTableEmptyRow'
 import { Template } from '~/queries/useQueryTemplates/fetch'
 import { getRole } from '~/utils'
+import { useState } from 'react'
 
 function ManagerTemplatesPage() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(5)
+
   const { user } = useUser()
   const templateModal = useTemplateModal()
   const { data: templates, isLoading, refetch } = useQueryTemplates()
@@ -24,15 +30,29 @@ function ManagerTemplatesPage() {
     templateModal.setMode('create')
   }
 
+  const startIndex = (currentPage - 1) * itemsPerPage
+  // startIndex = (1 -1) * 2 = 0
+  // startIndex = (2-1) *  2 = 2
+  // startIndex = (3-1) *  2  = 4
+  // array[0,1,2,3,4,5] => 0-1 2-3 4
+
+  const visibleTemplates = templates?.slice(startIndex, startIndex + itemsPerPage)
+  //Total pages need to be display
+  // array 5 item => 5/2 = 2,2,1
+  const totalPages = Math.ceil((templates?.length || 0) / itemsPerPage)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
   return (
     <div className="w-full flex flex-col gap-y-4">
-      <div className="flex flex-row items-center justify-between">
-        <h1 className="text-[22px] lg:text-2xl font-normal">Manager templates</h1>
+      <div className="flex  flex-col  sm:flex-row  items-center  justify-between">
+        <h1 className="text-[22px] sm:text-2xl font-normal sm:mb-0 ">Manager templates</h1>
         <button
           className="flex items-center justify-center bg-blue-500 text-white px-2.5 py-2 lg:px-3 lg:py-2.5 rounded-md active:scale-95 transition-transform duration-300"
           onClick={handleCreateNewTemplate}
         >
-          <FaPlus className="w-[16px] h-[16px] lg:w-[18px] lg:h-[18px] mr-1" />
+          <FaPlus className="w-[16px] h-[16px] lg:w-[18px] lg:h-[18px] mr-1 flex" />
           <span>New template</span>
         </button>
       </div>
@@ -75,7 +95,7 @@ function ManagerTemplatesPage() {
                 </td>
               </tr>
             ) : (templates as Template[]).length > 0 ? (
-              templates?.map((template) => (
+              visibleTemplates?.map((template) => (
                 <TemplateTableRow key={template.id} template={template} onRefetch={refetch} />
               ))
             ) : (
@@ -83,6 +103,34 @@ function ManagerTemplatesPage() {
             )}
           </tbody>
         </table>
+        <div className="flex justify-center py-2 px-2">
+          {/* Click button */}
+          <button
+            className={` mt-2 mr-2 flex justify-center items-center bg-blue-500 text-white w-6 h-6 rounded-full ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <IoIosArrowBack />
+          </button>
+          {/* Render numberPage */}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              className={`mt-2 px-2  ${currentPage === index + 1 ? 'font-bold text-white bg-blue-500 border flex justify-center' : ''} `}
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              disabled={currentPage === index + 1}
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            className={` mt-2 ml-2 flex justify-center items-center bg-blue-500 text-white w-6 h-6 rounded-full ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <IoIosArrowForward />
+          </button>
+        </div>
       </div>
     </div>
   )
