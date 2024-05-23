789094b559cc9df8ce34c284d19c7de3cf958743
Ngoc Dung <Phamdung11az@gmail.com>
2024-05-23 15:42:54 +0700

    ui: responsive, pagination

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -57,7 +57,7 @@ function Navbar() {
   }, [user, role, menu])
 
   return (
-    <aside className="p-4 flex flex-col gap-y-3 lg:basis-1/5 sm:hidden md:block">
+    <aside className="p-4 flex flex-col gap-y-3 lg:basis-1/5 sm:hidden md:block border-r border-r-gray-300">
       {MENU_ITEMS.map(({ id, to, title, icon }) => (
         <NavbarItem key={id} to={to} title={title} icon={icon} />
       ))}
--- src/pages/MyAvatars/index.tsx
+++ src/pages/MyAvatars/index.tsx
@@ -4,15 +4,39 @@ import Spinner from '~/components/Spinner'
 import AvatarTableRow from './AvatarTableRow'
 import AvatarTableEmpty from './AvatarTableEmpty'
 import { useUser } from '~/hooks'
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 function MyAvatars() {
   const { accessToken } = useUser()
   const { data: myAvatars, isLoading, refetch } = useQueryMyAvatars(accessToken ?? '')
 
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage] = useState(5)
+
+  const startIndex = (currentPage - 1) * itemsPerPage
+
+  const endIndex = startIndex + itemsPerPage
+
+  const visibleAvatars = myAvatars?.slice(startIndex, endIndex)
+
+  const totalPages = Math.ceil((myAvatars?.length || 0) / itemsPerPage)
+
+  useEffect(() => {
+    setCurrentPage(1)
+  }, [myAvatars])
+
+  const handlePageChange = (page: number) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page)
+    }
+  }
+
   return (
     <div className="select-none">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg  bg-gray-300">
-        <table className="table-auto w-full text-sm text-left rtl:text-right text-gray-600">
+        <table className="table-auto sm:w-full text-sm text-left rtl:text-right text-gray-600">
           <thead className="text-xs text-center text-white uppercase bg-[#0e64f1]">
             <tr>
               <th scope="col" className="px-6 py-3">
@@ -40,7 +64,7 @@ function MyAvatars() {
                 </th>
               </tr>
             ) : (myAvatars as MyAvatar[]).length > 0 ? (
-              myAvatars?.map((avatar) => (
+              visibleAvatars?.map((avatar) => (
                 <AvatarTableRow
                   key={avatar.id}
                   id={avatar.id}
@@ -57,6 +81,43 @@ function MyAvatars() {
             )}
           </tbody>
         </table>
+        <div className="flex justify-end bg-white p-2 ">
+          {/* Previous Page Button */}
+          <button
+            className={clsx(
+              'mt-2 flex justify-center items-center border border-gray-500 hover:bg-gray-300 text-gray-900 w-6 h-6 rounded-full',
+              { 'opacity-50 cursor-not-allowed': currentPage === 1 }
+            )}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <IoIosArrowBack />
+          </button>
+          {/* Render page number buttons */}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              className={clsx('mt-2 px-2 ml-3 hover:bg-blue-300 rounded-full hover:text-white', {
+                'font-medium text-white bg-blue-500': currentPage === index + 1
+              })}
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              disabled={currentPage === index + 1}
+            >
+              {index + 1}
+            </button>
+          ))}
+          {/* Next Page Button */}
+          <button
+            className={clsx(
+              'mt-2 ml-3 flex justify-center items-center border border-gray-500 hover:bg-gray-300 text-gray-900 w-6 h-6 rounded-full',
+              { 'opacity-50 cursor-not-allowed': currentPage === totalPages }
+            )}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <IoIosArrowForward />
+          </button>
+        </div>
       </div>
     </div>
   )
