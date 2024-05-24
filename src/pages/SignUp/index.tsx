fc9aa4cf37d8c861dceb35a45625cdbd77744bf3
Ngoc Dung <Phamdung11az@gmail.com>
2024-05-24 13:42:47 +0700

    feature responsive and pagination

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
 import { FieldValues, useForm } from 'react-hook-form'
-import { CiMail, CiUser } from 'react-icons/ci'
 import { IoEyeOutline } from 'react-icons/io5'
-import { IoEyeOffOutline, IoKeyOutline } from 'react-icons/io5'
+import { IoEyeOffOutline } from 'react-icons/io5'
 
 import { useRouter } from '~/hooks'
 import config, { supabase } from '~/config'
@@ -53,87 +52,95 @@ function SignUpPage() {
   }
 
   return (
-    <div className="flex justify-center h-screen items-center w-full">
-      <form
-        className="px-8 pt-6 pb-6 mb-4 w-96 bg-[#eff1f2] rounded-lg"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h2 className="text-2xl mb-4 text-center font-bold text-[#0e64f1]">Sign up</h2>
-        <div className="mb-4">
-          <label className="block text-sm mb-2 font-medium">Full name</label>
-          <div className="flex items-center bg-white rounded-lg pl-2">
-            <div>
-              <CiUser color="gray" size={20} />
-            </div>
-            <input
-              type="text"
-              disabled={loading}
-              autoComplete="off"
-              spellCheck="false"
-              placeholder="Enter your full name"
-              className="appearance-none w-full py-2 px-3 text-gray-700 rounded-lg outline-none"
-              {...register('fullName', { required: true })}
-            />
+    <div className="w-full h-screen flex items-center bg-gray-100 p-3">
+      <div className="w-[900px] h-[550px] m-auto bg-white p-3 rounded-3xl flex flex-row justify-center gap-3 shadow-xl">
+        <form className="bg-white w-full p-4" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <h1 className="text-2xl font-medium mb-2">Sign up here!</h1>
+            <h3 className=" text-[#72747c] mb-8">
+              Get started with our quick and easy registration process.
+            </h3>
           </div>
-          {errors.fullName && (
-            <p className="text-red-500 text-xs italic mt-2">Full name is required!</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm mb-2 font-medium">Email</label>
-          <div className="flex items-center bg-white rounded-lg pl-2">
-            <div>
-              <CiMail color="gray" size={20} />
+          <div>
+            <div className="mb-3">
+              <div className="flex flex-row">
+                <label className="block mb-1 font-medium">Full name</label>
+                <p className="ml-1 text-red-500">*</p>
+              </div>
+              <input
+                disabled={loading}
+                spellCheck="false"
+                autoComplete="off"
+                type="text"
+                className="w-full border border-gray-400 px-3 py-2 rounded-lg outline-none appearance-none"
+                placeholder="Michael Jackson"
+                {...register('fullName', { required: true })}
+              />
+              {errors.fullName && (
+                <p className="text-red-500 text-xs italic">Full name is required!</p>
+              )}
             </div>
-            <input
-              autoComplete="off"
-              disabled={loading}
-              placeholder="Enter your email "
-              type="text"
-              spellCheck="false"
-              className="appearance-none w-full py-2 px-3 text-gray-700 rounded-lg outline-none"
-              {...register('email', { required: true })}
-            />
-          </div>
-          {errors.email && <p className="text-red-500 text-xs italic mt-2">Email is required!</p>}
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm mb-2 font-medium">Password</label>
-          <div className="flex items-center bg-white rounded-lg pl-2">
-            <div>
-              <IoKeyOutline color="gray" size={20} />
+
+            <div className="mb-3">
+              <div className="flex flex-row">
+                <label className="block mb-1 font-medium">Email</label>
+                <p className="ml-1 text-red-500">*</p>
+              </div>
+              <input
+                disabled={loading}
+                autoComplete="off"
+                type="text"
+                className="w-full border border-gray-400 px-3 py-2 rounded-lg outline-none"
+                placeholder="example@gmail.com"
+                {...register('email', { required: true })}
+              />
+              {errors.email && <p className="text-red-500 text-xs italic">Email is required!</p>}
             </div>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              disabled={loading}
-              autoComplete="off"
-              spellCheck="false"
-              placeholder="Enter your password"
-              className="appearance-none w-full py-2 px-3 text-gray-700 rounded-lg outline-none"
-              {...register('password', { required: true })}
-            />
-            <div className="pr-2" onClick={togglePassword}>
-              {showPassword ? (
-                <IoEyeOutline color="gray" size={20} />
-              ) : (
-                <IoEyeOffOutline color="gray" size={20} />
+
+            <div className="relative">
+              <div className="flex flex-row">
+                <label className="block mb-1 font-medium">Password</label>
+                <p className="ml-1 text-red-500">*</p>
+              </div>
+              <div className="flex items-center border border-gray-400 rounded-lg">
+                <input
+                  disabled={loading}
+                  autoComplete="off"
+                  type={showPassword ? 'text' : 'password'}
+                  className="w-full px-3 py-2 rounded-lg outline-none"
+                  placeholder="Must have at least 6 characters"
+                  {...register('password', { required: true })}
+                />
+                <div className="pr-2" onClick={togglePassword}>
+                  {showPassword ? (
+                    <IoEyeOutline color="gray" size={20} />
+                  ) : (
+                    <IoEyeOffOutline color="gray" size={20} />
+                  )}
+                </div>
+              </div>
+              {errors.password && (
+                <p className="text-red-500 text-xs italic">Password is required!</p>
               )}
             </div>
           </div>
-          {errors.password && (
-            <p className="text-red-500 text-xs italic mt-2">Password is required!</p>
-          )}
+          <button className="w-full px-3 py-2 border border-blue-500 mt-8 rounded-lg bg-blue-500 text-white hover:bg-blue-700">
+            {loading ? <Spinner className="w-6 h-6 m-auto" /> : 'Sign up'}
+          </button>
+          <div className="mt-3 flex flex-row justify-center">
+            <span>Already have an account?</span>
+            <Link
+              to={config.routes.signIn}
+              className="text-blue-500 ml-1 cursor-pointer hover:underline"
+            >
+              Sign in
+            </Link>
+          </div>
+        </form>
+        <div className="w-full rounded-xl p-2 m-auto">
+          <img src="/assets/avatar-customize.png" className="rounded-lg shadow-lg" />
         </div>
-        <button className="py-2 px-4 mb-2 w-full text-center bg-[#0e64f1] text-white rounded-lg justify-center flex">
-          {loading ? <Spinner className="w-5 h-5" /> : 'Sign up'}
-        </button>
-        <p className="text-center text-sm">
-          Already have an account?
-          <Link to={config.routes.signIn} className="ml-1 text-[#0e64f1]">
-            Sign in
-          </Link>
-        </p>
-      </form>
+      </div>
     </div>
   )
 }
